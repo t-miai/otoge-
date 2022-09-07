1481bfa0199069e263bc6eea51c47d587a8c5716
t-miai <g20913im@gm.tsuda.ac.jp>
2022-09-07 13:56:41 +0900

    scenechange
    シーン切り替え

---
+++ kadai8_copy/Assets/Script/ButtonScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonScript : MonoBehaviour
+{
+    public void OnClick(){
+        Debug.Log("押された！");
+        SceneManager.LoadScene ("SampleScene");
+    }
+}
